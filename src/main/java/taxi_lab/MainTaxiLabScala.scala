1c0412765771c7376b180f9faa24ca993946b25e
AnafaHyams <anafa18@gmail.com>
2021-05-21 14:35:17 +0300

    Homework: Taxi lab - Extract to methods

--- src/main/java/taxi_lab/MainTaxiLabScala.scala
+++ src/main/java/taxi_lab/MainTaxiLabScala.scala
@@ -22,33 +22,24 @@ object MainTaxiLabScala {
     sc.setLogLevel("WARN")
 
     val rdd = sc.textFile("data/taxi/trips.txt")
+    val driversRdd = sc.textFile("data/taxi/drivers.txt")
 
-    val lines = rdd.count()
-    println(s"Number of lines: $lines")
+    countAndPrintNumberOfLines(rdd)
+    separateLine()
+    countAndPrintAmountTripsToBostonLongerThan10Km(rdd)
+    separateLine()
+    calculateAndPrintSumOfAllKmToCity(rdd, "BOSTON")
+    separateLine()
+    printNamesOf3DriversWithMaxTotalKm(sc, rdd, driversRdd)
 
-    val value: RDD[Array[String]] = rdd.map(line => line.split(" "))
-    val value1: RDD[Trip] = value.map(x => Trip(Integer.parseInt(x(0)), x(1).toUpperCase, Integer.parseInt(x(2)), LocalDate.now()))
-    val value2: RDD[Trip] = value1.filter(trip => trip.city == "BOSTON" && trip.distance > 10)
-    val bostonTripMoreThan10 = value2.count()
-    println(s"Number of trips to Boston longer than 10KM: $bostonTripMoreThan10")
+  }
 
-
-    val totalKilometersTripsToBoston = rdd.map(line => line.split(" "))
-      .map(x => Trip(Integer.parseInt(x(0)), x(1).toUpperCase, Integer.parseInt(x(2)), LocalDate.now()))
-      .filter(trip => trip.city == "BOSTON")
-      .map(_.distance)
-      .sum()
-
-    println(s"Total kilometers of trips to Boston: $totalKilometersTripsToBoston")
-
+  private def printNamesOf3DriversWithMaxTotalKm(sc: SparkContext, rdd: RDD[String], driversRdd: RDD[String]): Unit = {
     val drivers_km: RDD[(Int, Int)] = rdd.map(line => line.split(" "))
       .map(x => Trip(Integer.parseInt(x(0)), x(1).toUpperCase, Integer.parseInt(x(2)), LocalDate.now()))
       .groupBy(t => t.driverID)
       .mapValues(list => list.map(trip => trip.distance).sum)
       .sortBy(_._2, ascending = false)
-      //.take(3).
-
-    val driversRdd = sc.textFile("data/taxi/drivers.txt")
 
     val value4: RDD[(Int, String)] = driversRdd.map(line => line.split(", "))
       .map(x => Driver(Integer.parseInt(x(0)), x(1), x(2), x(3)))
@@ -58,7 +49,33 @@ object MainTaxiLabScala {
     println("Name of 3 drivers with max total kilometers:")
     drivers_km.join(value4).take(3)
       .foreach(tuple => println(tuple._2._2))
+  }
 
+  private def calculateAndPrintSumOfAllKmToCity(rdd: RDD[String], cityName: String): Unit = {
+    val totalKilometersTripsToBoston = rdd.map(line => line.split(" "))
+      .map(x => Trip(Integer.parseInt(x(0)), x(1).toUpperCase, Integer.parseInt(x(2)), LocalDate.now()))
+      .filter(trip => trip.city == cityName)
+      .map(_.distance)
+      .sum()
+
+    println(s"Total kilometers of trips to Boston: $totalKilometersTripsToBoston")
+  }
+
+  private def countAndPrintAmountTripsToBostonLongerThan10Km(rdd: RDD[String]): Unit = {
+    val value: RDD[Array[String]] = rdd.map(line => line.split(" "))
+    val value1: RDD[Trip] = value.map(x => Trip(Integer.parseInt(x(0)), x(1).toUpperCase, Integer.parseInt(x(2)), LocalDate.now()))
+    val value2: RDD[Trip] = value1.filter(trip => trip.city == "BOSTON" && trip.distance > 10)
+    val bostonTripMoreThan10 = value2.count()
+    println(s"Number of trips to Boston longer than 10KM: $bostonTripMoreThan10")
+  }
+
+  private def countAndPrintNumberOfLines(rdd: RDD[String]): Unit = {
+    val lines = rdd.count()
+    println(s"Number of lines: $lines")
+  }
+
+  private def separateLine(): Unit = {
+    println("-------------------------------------------------------------------")
   }
 
 }
